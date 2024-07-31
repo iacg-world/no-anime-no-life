919455e3e1fa66d567fa696fec0a689ecd919df4
lc <1033581609@qq.com>
2024-07-31 12:58:01 +0800

    feat: api cat=2

--- no-anime-no-life-be/src/search.service.ts
+++ no-anime-no-life-be/src/search.service.ts
@@ -14,7 +14,7 @@ export class SearchService {
 
   async findAll(keyword: string): Promise<SearchResponse> {
     const { data } = await firstValueFrom(
-      this.httpService.get<SearchResponse>(`https://api.bgm.tv/search/subject/${keyword}?type=2&max_results=25&start=1`, {
+      this.httpService.get<SearchResponse>(`https://api.bgm.tv/search/subject/${keyword}?cat=2&max_results=25&start=1`, {
         headers: {
           'User-Agent': 'iacg-world/no-anime-no-life',
           'Content-Type': 'application/json'
