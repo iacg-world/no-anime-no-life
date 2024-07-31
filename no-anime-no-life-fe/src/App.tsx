fe4a0ff61af444d9480b133d6f8fdd2e0ce96597
lc <1033581609@qq.com>
2024-07-31 12:03:01 +0800

    style: style perf

--- no-anime-no-life-fe/src/App.tsx
+++ no-anime-no-life-fe/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
+    <div className="flex flex-col h-screen overflow-hidden box-border bg-[#FFFAFA] lg:px-24">
       <div className="text-center h-[10vh]">
         <div className="text-center text-xl font-bold">NO ANIME NO LIFE</div>
         <div className="text-center font-bold">动画人生分享器</div>
@@ -14,7 +14,10 @@ function App() {
       <div className="grow h-[85vh]">
         <AnimeCategoryList/>
       </div>
-      <div className="h-[5vh]"></div>
+      <div className="h-[5vh] text-center text-xs text-gray-400">
+        <a target='_blank' className="mr-1" href="https://github.com/iacg-world/no-anime-no-life">@Github</a>
+        <a target='_blank' href="https://github.com/bangumi/api">动画接口来源</a>
+      </div>
     </div>
   )
 }
