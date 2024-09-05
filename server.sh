38fae9b4ef43e2bce575437c7f982c520caf9775
lc <1033581609@qq.com>
2024-09-05 15:09:47 +0800

    chore bash

--- server.sh
+++ server.sh
@@ -4,10 +4,7 @@
 app_name="nanf"
  
 # 检查应用是否在PM2列表中
-if pm2 list | grep -q $app_name; then
-  # 应用存在，重启
-  pm2 restart $app_name
+if npm run server; then
 else
-  # 应用不存在，启动
-  npm run server
+  pm2 restart $app_name
 fi
